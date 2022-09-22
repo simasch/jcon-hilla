02d85e024e6df292b10b1e741cb146f5813efb2c
Simon Martinelli <simon@72.services>
2022-09-22 11:32:54 +0200

    Formatting and refactoring

--- src/main/java/ch/martinelli/demo/data/entity/SamplePerson.java
+++ src/main/java/ch/martinelli/demo/data/entity/SamplePerson.java
@@ -1,13 +1,22 @@
 package ch.martinelli.demo.data.entity;
 
 import dev.hilla.Nonnull;
+import org.hibernate.annotations.Type;
+
 import java.time.LocalDate;
+import java.util.UUID;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 
 @Entity
-public class SamplePerson extends AbstractEntity {
+public class SamplePerson {
 
+    @Id
+    @GeneratedValue
+    @Type(type = "uuid-char")
+    private UUID id;
     @Nonnull
     private String firstName;
     @Nonnull
@@ -23,45 +32,66 @@ public class SamplePerson extends AbstractEntity {
     @Nonnull
     private boolean important;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
+
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
+
     public String getOccupation() {
         return occupation;
     }
+
     public void setOccupation(String occupation) {
         this.occupation = occupation;
     }
+
     public boolean isImportant() {
         return important;
     }
+
     public void setImportant(boolean important) {
         this.important = important;
     }
